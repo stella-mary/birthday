daa4b9da8436d204abebc8136976c11e11385923
stella-mary <mariastellamary@outlook.com>
2022-10-12 17:14:46 +0530

    New File

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        id: 1,
+        name: 'Sivakrathikeyan',
+        age: 37,
+        image: 'http://www.behindwoods.com/tamil-movies/slideshow/official-cast-and-crew-details-of-sivakrathikeyans-next/images/hero-sivakarthikeyan.jpg',
+    },
+    {
+        id: 2,
+        name: 'Suriya',
+        age: 47,
+        image: 'https://wallpapercave.com/wp/wp2581607.jpg',
+    },
+    {
+        id: 3,
+        name: 'Simbu',
+        age: 39,
+        image: 'http://4.bp.blogspot.com/--mxZD9u7HfA/UkaRAvqIMwI/AAAAAAAAAnI/Xv51E1FrYbA/s1600/simbu.jpg',
+    },
+    {
+        id: 4,
+        name: 'Vijay',
+        age: 48,
+        image: 'https://media.indiatimes.in/media/content/2017/Oct/2_1508824766_725x725.jpg',
+    },
+    {
+        id: 5,
+        name: 'Dhanush',
+        age: 39,
+        image: 'https://www.scooptimes.com/wp-content/uploads/dhanush-biography.jpg',
+    },
+
+
+]
